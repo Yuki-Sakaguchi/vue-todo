22dc5a28f8eb44876b403bccd70e5bcb51d69f82
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2018-11-25 02:32:50 +0900

    mod

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,82 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <div id="contents">
+      <transition name="router-transition">
+        <router-view/>
+      </transition>
+    </div>
   </div>
 </template>
 
+
 <style lang="scss">
+$main_color: #203143;
+$sidebar_size: 300px;
+
+* {
+  box-sizing: border-box;
+}
+
+html,
+body,
+#app {
+  width: 100%;
+  height: 100%;
+}
+
+body {
+  border-top: 10px solid $main_color;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: flex;
 }
+
 #nav {
+  width: $sidebar_size;
+  height: 100%;
   padding: 30px;
+  background-color: $main_color;
+  color: white;
+
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: white;
+    display: block;
+    margin: 15px 0;
+
     &.router-link-exact-active {
       color: #42b983;
     }
   }
+}
+
+#contents {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.router-transition-enter-active {
+  transition: all .3s ease .3s;
+}
+.router-transition-leave-active {
+  position: absolute;
+  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.router-transition-enter,
+.router-transition-leave-to {
+  transform: translateX(10px);
+  opacity: 0;
 }
 </style>
